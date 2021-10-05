73180db7fa67564837b9a3252748dc8469f0d991
nonoka57 <nonoka57nonoka@gmail.com>
2021-10-05 10:31:54 +0000

    [add]ルートとか・・・

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,36 @@
 class BooksController < ApplicationController
+
   def index
+    @books = Book.all
   end
 
   def show
+    @books = Book.find(params[:id])
   end
 
-  def edit
+  def new
+    @book=Book.new
   end
+
+  def create
+    @book=Book.new(book_params)
+    if @book.save
+    redirect_to home_path
+    else
+    render index
+    end
+  end
+
+
+  def edit
+    @books = Book.find(params[:id])
+    @books.save
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:books).permit(:title, :body)
+  end
+
 end
