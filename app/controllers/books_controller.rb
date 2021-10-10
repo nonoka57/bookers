bf7aeb00bf9873c348b18aa7300e4290cd827e87
nonoka57 <nonoka57nonoka@gmail.com>
2021-10-10 04:18:40 +0000

    [add]flash

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,7 +16,8 @@ class BooksController < ApplicationController
   def create
     @book=Book.new(book_params)
     if @book.save
-    redirect_to book_path(@book.id)
+      flash[:notice]="Book was successfully created."
+      redirect_to book_path(@book.id)
     else
     @books = Book.all
     render :index
@@ -31,6 +32,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
       redirect_to book_path(@book.id)
     else
       render :edit
@@ -40,6 +42,7 @@ class BooksController < ApplicationController
   def destroy
     book=Book.find(params[:id])
     book.destroy
+    flash[:notice]="Book was successfully destroyed."
     redirect_to books_path
   end
 
